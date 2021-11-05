20796989001fb583ced84cbd587640c56a1fd4cb
mreyaz94 <mdreyaz94@gmail.com>
2021-11-05 08:18:33 +0530

    make it better

--- NurtureLabAPI/settings.py
+++ NurtureLabAPI/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'wqo&5mdp2ap0j1oy801*p3mz-2ui2i+iy30@#sq13p#)q8+2z$'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["nurturelabapi.herokuapp.com"]
+ALLOWED_HOSTS = ["nurturapi.herokuapp.com"]
 
 
 # Application definition
